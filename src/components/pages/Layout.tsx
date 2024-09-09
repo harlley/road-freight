be3e4aeebee75cb74a372127644fd2d356f48525
Harlley Oliveira <harlley@gmail.com>
2024-09-09 16:59:37 +0100

    install material ui

--- src/components/pages/Layout.tsx
+++ src/components/pages/Layout.tsx
@@ -1,8 +1,17 @@
 import { Outlet } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Button from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/Save";
 
 export function Layout() {
   return (
     <>
+      <Button variant="contained" startIcon={<SaveIcon />}>
+        Save
+      </Button>
       <div id="sidebar">
         <nav>
           <ul>
